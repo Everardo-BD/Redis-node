339c7813ad188b2cfd5754e041facabb8baf7520
Everardo <A01552016@itesm.mx>
2020-10-31 16:55:04 -0600

    first

---
+++ app.js
@@ -0,0 +1,21 @@
+var redis = require('redis')
+
+var client = redis.createClient(6379,'104.198.244.0');
+
+client.on('connect', function(){
+	console.log('connectado');
+});
+
+
+client.exists('key', function(err, reply) {
+    if (reply === 1) {
+        console.log('exists');
+        client.get('query1', function(err, reply) {
+   	 	console.log(reply);
+	});
+    } else {
+        console.log('doesn\'t exist');
+	client.set('query1','{"id":1, "name":"ever"}')
+    }
+});
+
